41b13f74285ee645b6530365a09593e97c250ab1
Nation.Jackes <ewan.delannoy91@gmail.com>
2021-09-28 10:37:02 +0200

    Premier commit du tp jeu
    Démarrage du projet

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+	private static Scanner clavier;
+		public static void main(String[] args) {
+			System.out.println("Saisir un nombre : ");
+			clavier = new Scanner(System.in);
+			int nombre = 0;
+			try {
+				nombre = clavier.nextInt();
+			}
+			catch(InputMismatchException e) {
+				System.out.println("BOOM,saisie incorrect, voici ton QI :");
+				
+			}
+			System.out.println(nombre);
+			}
+	
+}
+
